d4ce3ca6f74ea46726138e4671456e24bcee8a82
diego pacori <dpacoria@unsa.edu.pe>
2022-06-23 01:45:24 -0500

    Se realizo hasta la parte de colocar todos los destinos en objetos y ubicarlos en una lista para ponerlo como contexto y enviarlo a index.html

---
+++ src/travello/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Destination
+# Create your views here.
+
+def index(request):
+
+    dest1 = Destination()
+    dest1.name = 'Mumba1'
+    dest1.price = 700
+    return render(request, 'index.html',{'dest1': dest1})
