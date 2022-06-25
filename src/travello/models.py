408de1b4e15d55dd341b97bc7cb2090843558396
diego pacori <dpacoria@unsa.edu.pe>
2022-06-24 23:23:09 -0500

    Se agrego la funcion para modificar, pero con un peuqe;o detalle, y es que tienes que agregar si o si la imagen , sino no te deja modificar, puede ser cualquier imagen y tambien puede ser la misma, es lo menos caotico que pude hacerlo

--- src/travello/models.py
+++ src/travello/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Destination(models.Model):
     
     name= models.CharField(max_length=100)
-    img= models.ImageField(upload_to='pics', null=True)
+    img= models.ImageField(upload_to='pics')
     desc= models.TextField()
     price= models.IntegerField()
     offer= models.BooleanField(default=False)
--- src/travello/views.py
+++ src/travello/views.py
@@ -5,20 +5,19 @@ from .models import Destination
 from .forms import DestinationForm
 # Create your views here.
 
-def addDestination(request):
+def addDestination(request, myID):
     form = DestinationForm(request.POST, request.FILES)
+    
     if form.is_valid():
-        print("Se ha guardado")
         form.save()
         form = DestinationForm()
         return redirect('/listarDestinos')
     else:
-        print("No se ha guardado")
         form = DestinationForm()
-    
     context = {
         'form' : form
     }
+    
     return render(request, 'addDestination.html',context)
 
 def listarDestinos(request):
@@ -32,17 +31,16 @@ def index(request):
     return render(request, 'index.html',{'Destin': dests})
 
 def modDestino(request, myID):
-    print(myID)
-    obj = Destination.objects.get(id = myID)
-    form = DestinationForm(request.POST or None, instance = obj)
-    
-    if form.is_valid():
-        print('Aqui estuvo')
-        form.save()
-        form = DestinationForm()
-        return redirect('/listarDestinos')
+    obj = Destination.objects.get(id=myID)
+    if request.method == 'POST':
+        form = DestinationForm(request.POST,request.FILES)
+        form.instance = obj
+        if form.is_valid():
+            form.save()
+            form = DestinationForm()
+            return redirect('/listarDestinos')
     else:
-        print('here!!')
+        form = DestinationForm(request.POST or None,instance=obj)
     context = {
         'form': form,
     }
